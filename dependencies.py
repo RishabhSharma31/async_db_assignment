7d5716dcab327e43d0bb573aa617afb93cf31929
RishabhSharma31 <114903535+RishabhSharma31@users.noreply.github.com>
2022-10-06 16:53:36 +0530

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,3 +1,5 @@
+#In order to remove the need for implementing DB session initialization for each endpoints, we use book_dal as a dependency of our endpoint.
+
 
 from database.config import async_session
 from database.dals.book_dal import BookDAL
